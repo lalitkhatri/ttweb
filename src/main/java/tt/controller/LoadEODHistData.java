6e2bc1f4db259340f961a8a1f2c4ee4c0b971e2a
ubuntu <ubuntu@ud2>
2022-02-14 22:43:32 +0000

    Integrated EOD Hist data load api

--- src/main/java/tt/controller/LoadEODHistData.java
+++ src/main/java/tt/controller/LoadEODHistData.java
@@ -1,35 +1,32 @@
 package tt.controller;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import loader.ViewTimeseries;
-import model.Record;
+import loader.EODHistDataLoader;
 
 @RestController
-@RequestMapping("/timeseries")
-public class Timeseries {
+@RequestMapping("/load")
+public class LoadEODHistData {
 
-	private ViewTimeseries viewtimeseries = new ViewTimeseries();
+	private EODHistDataLoader dataLoader = new EODHistDataLoader();
 	
 	@CrossOrigin(origins = "http://localhost:4040")
 	@GetMapping("/{exchange}/{symbol}")
-	public Map<String, Record> getTimeseries(@PathVariable("exchange") String exchange, @PathVariable("symbol") String symbol) throws Exception {
-		System.out.println("Get Timeseries for - "+ symbol);
-		return viewtimeseries.getTimeseries(exchange.toUpperCase(), symbol.toUpperCase());
+	public void loadData(@PathVariable("exchange") String exchange, @PathVariable("symbol") String symbol) throws Exception {
+		System.out.println("Loading data for - "+ symbol);
+		dataLoader.loadData(exchange.toUpperCase(), symbol.toUpperCase());
 	}
 	
 	@CrossOrigin(origins = "http://localhost:4040")
 	@GetMapping("/{exchange}/{symbol}/{freq}")
-	public Map<String, Record> getTimeseries(@PathVariable("exchange") String exchange, 
+	public void loadData(@PathVariable("exchange") String exchange, 
 			@PathVariable("symbol") String symbol, @PathVariable("freq") String freq) throws Exception {
 		System.out.println("Get Timeseries for symbol - "+ symbol + " and freq - "+ freq);
-		return viewtimeseries.getTimeseries(exchange.toUpperCase(), symbol.toUpperCase(),freq.toUpperCase());
+		dataLoader.loadData(exchange.toUpperCase(), symbol.toUpperCase(),freq.toLowerCase());
 	}
-
+	
 }
